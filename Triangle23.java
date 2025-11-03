994cc9f6d3fc8a7aca64d1bc79e1ba6daa99fda6
Yann <yann@gmail.com>
2025-11-03 08:17:22 +0700

    2.3 Percobaan 3 : Bintang Segitiga

---
+++ Triangle23.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Triangle23 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("MAsukkan nilai N = ");
+        int N = sc.nextInt();
+
+        int i = 1;
+        while (i <= N) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
